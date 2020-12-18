20558a823e965441b2088619ac0724aacf42ea20
codessi <johansuh@gmail.com>
2020-12-18 15:54:54 -0800

    initial commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const alwaysOptions = (
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      rcode-scramble-client
+      code-scramble-client
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
