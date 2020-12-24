53372217bab8dee39d5aff1c0067cb6a690b2774
codessi <johansuh@gmail.com>
2020-12-24 10:13:39 -0800

    change white-space attribute

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,7 +27,7 @@ const alwaysOptions = (
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      code-scramble-client
+      Code-Scramble
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
